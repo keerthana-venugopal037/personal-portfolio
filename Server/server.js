527bdbaf155f2ec6f639b2ff547caca2109ee061
Keerthana <keerthanavp.2k7@gmail.com>
2026-08-05 12:43:51 +0530

    Fix Client folder path

--- Server/server.js
+++ Server/server.js
@@ -3,9 +3,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, "../client")));
+app.use(express.static(path.join(__dirname, "../Client")));
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/index.html"));
+    res.sendFile(path.join(__dirname, "../Client/index.html"));
 });
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
